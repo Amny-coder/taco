3c9eb7df8ad1105603970c0000d8174a944d8f85
amny.inc <amny.inc@gmail.com>
2023-08-09 11:11:13 +0100

    feat: added argument constructor

--- src/main/java/inc/amny/taco/Ingredient.java
+++ src/main/java/inc/amny/taco/Ingredient.java
@@ -1,13 +1,23 @@
 package inc.amny.taco;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class Ingredient {
 
     private String id;
+
     private String name;
+
     private Type type;
+
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
