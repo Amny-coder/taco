a33f986644fcd25db83aabcdc38099ff560f37c7
amny.inc <amny.inc@gmail.com>
2023-08-11 12:23:52 +0100

    fix: handling post request approparately

--- src/main/java/inc/amny/taco/web/OrderController.java
+++ src/main/java/inc/amny/taco/web/OrderController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -21,6 +22,7 @@ public class OrderController {
         return "orderForm";
     }
 
+    @PostMapping
     public String processOrder(@Valid TacoOrder order, Errors errors,
                                SessionStatus status) {
         if (errors.hasErrors()) {
