9447e733a8da50a883a73d0c7fef6da9bf5585eb
amny.inc <amny.inc@gmail.com>
2023-08-21 17:08:27 +0100

    feat: added spring security support

--- src/main/java/inc/amny/taco/data/OrderRepository.java
+++ src/main/java/inc/amny/taco/data/OrderRepository.java
@@ -1,9 +1,8 @@
 package inc.amny.taco.data;
 
-import inc.amny.taco.Taco;
 import inc.amny.taco.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository extends CrudRepository<Taco, Long> {
+public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
 
 }
